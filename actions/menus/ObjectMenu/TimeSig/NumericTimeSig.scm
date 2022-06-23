ed4c27fdaed30693b79930193e966613c629f668
Richard Shann <richard@rshann.plus.com>
2022-06-23 14:24:21 +0100

    apply numeric time signature to all staffs

--- actions/menus/ObjectMenu/TimeSig/NumericTimeSig.scm
+++ actions/menus/ObjectMenu/TimeSig/NumericTimeSig.scm
@@ -1,5 +1,9 @@
 ;;;NumericTimeSig
-(let ((tag "NumericTimeSig"))
+(let ((tag "NumericTimeSig")(params NumericTimeSig::params))
+	(if (not params)
+		(begin
+			(d-PushPosition)
+			(d-GoToPosition #f 1 #f 1)))
   (if (d-Directive-timesig? tag)
     (d-DirectiveDelete-timesig tag)
     (begin
@@ -7,6 +11,12 @@
       (d-DirectivePut-timesig-graphic tag "\nN\nDenemo\n12")
       (d-DirectivePut-timesig-gy tag 0)
       (d-DirectivePut-timesig-minpixels tag 30)))
+
   
-(d-SetSaved #f)
-(d-RefreshDisplay))
+  (while (MoveDownStaffOrVoice)
+  	(d-NumericTimeSig 'recursive))
+ (if (not params)
+	(begin
+		(d-PopPosition)
+		(d-SetSaved #f)
+		(d-RefreshDisplay))))
