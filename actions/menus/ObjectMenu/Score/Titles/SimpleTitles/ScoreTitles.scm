4984fddd63c8c2972bd837d857602a76ecf83c98
Richard Shann <richard@rshann.plus.com>
2022-07-05 16:48:18 +0100

    Warn about clash between tagline and simple titles

--- actions/menus/ObjectMenu/Score/Titles/SimpleTitles/ScoreTitles.scm
+++ actions/menus/ObjectMenu/Score/Titles/SimpleTitles/ScoreTitles.scm
@@ -1,2 +1,10 @@
 ;;;SetMovementTitles
-(DenemoSetTitles "ScoreTitles" #f #f)
+(let ((tagline (d-DirectiveGet-scoreheader-display "ScoreTagline")))
+	(if tagline
+		(begin 
+			(d-PutTextClipboard tagline)
+			(d-DirectiveDelete-scoreheader "ScoreTagline")
+			(d-WarningDialog (string-append (_ "You had set the score tagline:\n") 
+					tagline 
+					(_ "\nThis is now copied to the clipboard\nIf you wish to keep it set it by pasting it in Simple Titles")))))
+	(DenemoSetTitles "ScoreTitles" #f #f))
