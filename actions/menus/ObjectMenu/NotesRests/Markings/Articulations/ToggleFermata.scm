6245ae37e4eceec5a62f7299d4eb8846e11cca39
Richard Shann <richard@rshann.plus.com>
2022-09-15 09:02:19 +0100

    Fix fermata for whole measure rests in newer LilyPond versions

--- actions/menus/ObjectMenu/NotesRests/Markings/Articulations/ToggleFermata.scm
+++ actions/menus/ObjectMenu/NotesRests/Markings/Articulations/ToggleFermata.scm
@@ -1,5 +1,2 @@
 ;;;ToggleFermata
-        
-(if (d-Directive-chord? "WholeMeasureRest")
-    (ChordAnnotation "ToggleFermata" "\\fermataMarkup"    ToggleFermata::params    LG-Fermata)
-    (ChordAnnotation "ToggleFermata" "\\fermata"    ToggleFermata::params    LG-Fermata))
+(ChordAnnotation "ToggleFermata" "\\fermata"    ToggleFermata::params    LG-Fermata)
