e4bf2ed59df45918083eb7ca2ccdbc2dc90d3090
Richard Shann <richard@rshann.plus.com>
2022-12-11 11:02:34 +0000

    Revert "Create AllowOpenStrings, bump version for new command"
    This reverts commit 9776a769dbff074a66877d7968e1d14d59d74c5c.

--- actions/menus/ObjectMenu/StaffMenu/StaffPropertiesMenu/Tablature/AllowOpenStrings.scm
+++
@@ -1,7 +0,0 @@
-;;AllowOpenStrings
-(let ((tag "AllowOpenStrings"))
-			(d-Directive-standalone tag)
-			(d-DirectivePut-standalone-postfix tag (string-append "\\set TabStaff.restrainOpenStrings=##f"))
-			(d-DirectivePut-standalone-display tag "+0")
-			(d-DirectivePut-standalone-minpixels tag 30)
-			(d-SetSaved #f)(d-RefreshDisplay))
