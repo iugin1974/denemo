862e00c14d7793e51c2b4bda2d0981d1473fa5cc
Richard Shann <richard@rshann.plus.com>
2022-12-11 11:03:29 +0000

    Revert "Correct set minimum fret script. Bump version for new command"
    This reverts commit 1cf193de604090d12f70847178fd73dfe57775cc.

--- actions/menus/ObjectMenu/StaffMenu/StaffPropertiesMenu/Tablature/SetMinimumFret.scm
+++ actions/menus/ObjectMenu/StaffMenu/StaffPropertiesMenu/Tablature/SetMinimumFret.scm
@@ -1,7 +1,7 @@
 ;;SetMinimumFret
 (let ((tag "SetMinimumFret")(fret (d-GetUserInput (_ "Tab Fret") (_ "Give Minimum Fret Number: ") "8")))
 	(if (and fret (string->number fret))
-		(let ((restrain (if (zero? (string->number fret)) "##f" "##t")))
+		(let ((restrain (if (zero? (string->number fret)) "#f" "#t")))
 			(d-Directive-standalone tag)
 			(d-DirectivePut-standalone-postfix tag (string-append "\\set TabStaff.minimumFret = #" fret " \\set TabStaff.restrainOpenStrings=" restrain " "))
 			(d-DirectivePut-standalone-display tag (string-append "fret>" fret))
