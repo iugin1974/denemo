2683236617cd845af31af09560668b44c5522197
Richard Shann <richard@rshann.plus.com>
2022-12-29 11:03:55 +0000

    Allow a parameter to avoid offering conditional behavior

--- actions/menus/ObjectMenu/TimeSig/HideTimesig.scm
+++ actions/menus/ObjectMenu/TimeSig/HideTimesig.scm
@@ -1,13 +1,13 @@
 ;;;HideTimesig
-(let ((tag "HideTimesig"))
+(let ((tag "HideTimesig")(params HideTimesig::params))
 	(if (Timesignature?)
 		(if (d-Directive-timesig? tag)
 		    (d-DirectiveDelete-timesig tag)
 		    (begin
 		        (d-DirectivePut-timesig-prefix tag  (string-append  "\\once \\override Staff.TimeSignature #'stencil = ##f"  ))
-                (d-DirectivePut-timesig-gy tag 60)
-                (d-DirectivePut-timesig-graphic tag "\n⋃\nDenemo\n24")
-				(SetDirectiveConditional "timesig" tag)))
+		        (d-DirectivePut-timesig-gy tag 60)
+		        (d-DirectivePut-timesig-graphic tag "\n⋃\nDenemo\n24")
+			(if (not params)(SetDirectiveConditional "timesig" tag))))
 		 (begin 
 			(if (d-Directive-timesig? tag)
 				(d-DirectiveDelete-timesig tag)
@@ -15,5 +15,5 @@
 					(d-DirectivePut-timesig-prefix tag  (string-append  "\\once \\override Staff.TimeSignature #'stencil = ##f"  ))
 					(d-DirectivePut-timesig-gy tag 60)
 					(d-DirectivePut-timesig-graphic tag "\n⋃\nDenemo\n24")
-					(SetDirectiveConditional "timesig" tag)))))
+					(if (not params)(SetDirectiveConditional "timesig" tag))))))
 	(d-SetSaved #f))
