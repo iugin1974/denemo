0b2e37fce0e0b5eef012e2309c2a1ac6fcbfd4c2
Richard Shann <richard@rshann.plus.com>
2023-01-02 12:16:21 +0000

    Add markup before movement command

---
+++ actions/menus/ObjectMenu/MovementMenu/Titles/MarkupAtStart.scm
@@ -0,0 +1,24 @@
+;;;MarkupAtStart
+(let* ((tag "MarkupAtStart")(themarkup #f)(data (d-DirectiveGet-movementcontrol-data tag)))
+(disp "eneter with " data "\n\n")
+        (d-DirectivePut-movementcontrol-override tag (logior
+            DENEMO_OVERRIDE_GRAPHIC DENEMO_OVERRIDE_TAGEDIT))
+        (d-DirectivePut-movementcontrol-display tag (_ "Markup At Start"))
+        (if data
+        (set! data (eval-string data))
+        (set! data '()))
+        
+        (disp "so data is " data "\n\n")
+        (set! themarkup (assq-ref data 'text))
+        (if (not themarkup)
+            (set! themarkup ""))
+        (set! themarkup (d-GetUserInputWithSnippets (_ "Markup At End") (_ "Edit markup:") themarkup))
+        (if themarkup
+            (begin
+                (set! data 
+                    (string-append "(list (cons 'text \" "  (scheme-escape (car themarkup))  "\"))"))
+                (d-DirectivePut-movementcontrol-data tag  data)
+                (set! themarkup (cdr themarkup))
+                (d-DirectivePut-movementcontrol-prefix tag (string-append "\\markup \\column { " themarkup " }"))
+                (d-SetSaved #f))
+            (d-InfoDialog "Cancelled")))
