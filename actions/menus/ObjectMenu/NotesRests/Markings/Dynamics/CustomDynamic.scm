f61d8837c2832aa9312a1562916404595141e3b6
Richard Shann <richard@rshann.plus.com>
2023-04-09 17:40:49 +0100

    Add command to attach custom dynamics to a note. Bump version

---
+++ actions/menus/ObjectMenu/NotesRests/Markings/Dynamics/CustomDynamic.scm
@@ -0,0 +1,46 @@
+;;;CustomDynamic
+(let ((text #f) (tag "CustomDynamic") (params CustomDynamic::params) (markup #f)(current #f)
+            (position #f) (shift ""))
+     (set! current (d-DirectiveGet-chord-data tag))
+     (if (not current)
+        (set! current (d-DirectiveGet-chord-display tag)))
+     (if (and params (not (equal? "edit" params)))
+        (begin
+            (if (string? params)
+                (set! text (cons params (string-append "\"" params "\"")))
+                (let ((choice (caar params)))  ;;; params is a list of pairs
+                     ;(set! text (cons current (string-append "\"" current "\""))) 
+                        (case choice
+                        ((padding)   (set! shift (string-append "-\\tweak #'padding #"  (cdar params) "  ")))
+                        ((direction)  (set! position (cdar params)))            
+                        ((offsetx)  (set! position (string-append "-\\tweak #'X-offset #'" (cdar params) " -\\tweak #'Y-offset #'"  (cdadr params) " -"))))))))
+      (if (not position)
+         (begin   
+            (set! position (RadioBoxMenuList (list (cons (_ "Above") "^")  (cons (_ "Below") "_") (cons (_ "Auto Position") "-"))))))
+            
+     (if (not current)
+                (set! current "")) 
+    (if (not text)
+        (set! text (d-GetUserInputWithSnippets (_ "Text") (_ "Give dynamic text to appear with note/chord:\nThe characters \\, \", §, { and } have a special meaning in the text,\nthe backslash \\ starts some LilyPond syntax, the others must be paired.\nRestrict use of the dynamic font to the letters mfprsz, see LilyPond documentation.") current)));;cannot popup menu after this, it runs gtk_main
+ 
+    (if text
+        (if (string-null? (car text))
+  
+            (begin
+                (let ((confirm (d-GetUserInput (d-DirectiveGet-chord-display tag) (_ "Delete this text?") (_ "y"))))
+                 (if (equal? confirm (_ "y"))
+                    (begin
+                        (d-DirectiveDelete-chord tag)
+                        (d-SetSaved #f))
+                    (d-InfoDialog (_ "Cancelled")))))
+            (begin 
+                (if position
+                   (begin
+                            (set! markup (cdr text))
+                            (set! text (car text))
+                            (d-DirectivePut-chord-display tag (string-pad-right text 5))
+                            (d-DirectivePut-chord-data tag text)
+                            (d-DirectivePut-chord-postfix tag (string-append shift position "#(make-dynamic-script #{ \\markup " markup " }#}) "))
+                            (d-DirectivePut-chord-minpixels tag 20)
+                            (d-SetSaved #f)))))))
+            
