f11fb52960bb954a5d708efe2228cbccbf6cdca7
Richard Shann <richard@rshann.plus.com>
2023-07-29 16:33:19 +0100

    Allow setting begin/end of line visibility for custom rehearsal mark

--- actions/menus/ObjectMenu/Directives/Markings/CustomRehearsalMark.scm
+++ actions/menus/ObjectMenu/Directives/Markings/CustomRehearsalMark.scm
@@ -13,14 +13,18 @@
         (set! value "ral."))
     (set! value (d-GetUserInputWithSnippets (_ "Text or Custom Rehearsal Mark") (_ "Give text to use for Mark") value))
     (if value   ;in case the user pressed Escape do nothing    
-        (let ((text (cdr value))(data (car value))(position (RadioBoxMenu (cons (_ "left") "left") (cons (_ "center") "center") (cons (_ "right") "right")))) 
+        (let ((text (cdr value))(data (car value))(position (RadioBoxMenu (cons (_ "left") "left") (cons (_ "center") "center") (cons (_ "right") "right")))
+        (eof (RadioBoxMenu (cons (_ "Visible at end of line") " \\tweak break-visibility  #begin-of-line-invisible ")
+			(cons (_ "Visible at start of line") " \\tweak break-visibility  #begin-of-line-visible ") (cons (_ "default") "")))
+        
+        ) 
          (if position
          	(begin
 		  (d-Directive-standalone tag)
 		  (d-DirectivePut-standalone-display tag data)
 		  (d-DirectivePut-standalone-graphic tag "RehearsalMark")
 		  (d-DirectivePut-standalone-gx tag 15)
-		  (d-DirectivePut-standalone-postfix tag  (string-append  " \\once \\override Score.RehearsalMark.self-alignment-X = #" position " \\mark \\markup \\column {" text "}" ) )
+		  (d-DirectivePut-standalone-postfix tag  (string-append  " \\once \\override Score.RehearsalMark.self-alignment-X = #" position eof " \\mark \\markup \\column {" text "}" ) )
 		  (d-DirectivePut-standalone-grob  tag  "RehearsalMark")
 		  (d-DirectivePut-standalone-minpixels  tag  30)
 		  (d-DirectivePut-standalone-data tag (string-append "(list (cons  'value \""  (scheme-escape data) "\"))"))
