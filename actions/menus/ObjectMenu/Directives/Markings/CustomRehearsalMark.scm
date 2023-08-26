6d4a8458024176f9497538608e134f588b81feea
Richard Shann <richard@rshann.plus.com>
2023-08-26 18:00:43 +0100

    Update custom rehearsal mark for lpond 2.24

--- actions/menus/ObjectMenu/Directives/Markings/CustomRehearsalMark.scm
+++ actions/menus/ObjectMenu/Directives/Markings/CustomRehearsalMark.scm
@@ -1,5 +1,5 @@
 ;;; CustomRehearsalMark
-(let ((value CustomRehearsalMark::params) (tag "CustomRehearsalMark"))
+(let ((value CustomRehearsalMark::params) (tag "CustomRehearsalMark")(newSyntax (d-CheckLilyVersion "2.24")))
     (if (equal? value "edit")
         (set! value #f))
     (if (and (not value) (d-Directive-standalone? tag))
@@ -13,21 +13,37 @@
         (set! value "ral."))
     (set! value (d-GetUserInputWithSnippets (_ "Text or Custom Rehearsal Mark") (_ "Give text to use for Mark") value))
     (if value   ;in case the user pressed Escape do nothing    
-        (let ((text (cdr value))(data (car value))(position (RadioBoxMenu (cons (_ "left") "left") (cons (_ "center") "center") (cons (_ "right") "right")))
-        (eof (RadioBoxMenu (cons (_ "Visible at end of line") " \\tweak break-visibility  #begin-of-line-invisible ")
-			(cons (_ "Visible at start of line") " \\tweak break-visibility  #begin-of-line-visible ") (cons (_ "default") "")))
-        
-        ) 
+        (let ((text (cdr value))
+			(data (car value))
+			(position (if newSyntax
+								""
+								(RadioBoxMenu (cons (_ "left") "LEFT") (cons (_ "center") "CENTER") (cons (_ "right") "RIGHT"))))
+			(eof (RadioBoxMenu (cons (_ "Visible at end of line") 'end)
+				(cons (_ "Visible at start of line") 'begin) (cons (_ "default") ""))) ) 
          (if position
          	(begin
-		  (d-Directive-standalone tag)
-		  (d-DirectivePut-standalone-display tag data)
-		  (d-DirectivePut-standalone-graphic tag "RehearsalMark")
-		  (d-DirectivePut-standalone-gx tag 15)
-		  (d-DirectivePut-standalone-postfix tag  (string-append  " \\once \\override Score.RehearsalMark.self-alignment-X = #" position eof " \\mark \\markup \\column {" text "}" ) )
-		  (d-DirectivePut-standalone-grob  tag  "RehearsalMark")
-		  (d-DirectivePut-standalone-minpixels  tag  30)
-		  (d-DirectivePut-standalone-data tag (string-append "(list (cons  'value \""  (scheme-escape data) "\"))"))
-		  (d-SetSaved #f)
-		  (d-RefreshDisplay))))))
-
+				(if newSyntax
+					(case eof
+						((begin)
+							(set! eof "\\textMark "))
+						(else
+							(set! eof "\\textEndMark")))
+					(begin
+						(case eof
+							((begin)
+								(set! eof  " \\tweak break-visibility  #begin-of-line-visible \\mark "))
+							((end)
+								(set! eof  " \\tweak break-visibility  #begin-of-line-invisible \\mark"))
+								(else 
+									(set! eof "\\mark ")))
+						(set! eof (string-append  " \\once \\override Score.RehearsalMark.self-alignment-X = #" position eof))))
+				(d-Directive-standalone tag)
+				(d-DirectivePut-standalone-display tag data)
+				(d-DirectivePut-standalone-graphic tag "RehearsalMark")
+				(d-DirectivePut-standalone-gx tag 15)
+				(d-DirectivePut-standalone-postfix tag  (string-append  eof "\\markup \\column {" text "}" ) )
+				(d-DirectivePut-standalone-grob  tag  "RehearsalMark")
+				(d-DirectivePut-standalone-minpixels  tag  30)
+				(d-DirectivePut-standalone-data tag (string-append "(list (cons  'value \""  (scheme-escape data) "\"))"))
+				(d-SetSaved #f)
+				(d-RefreshDisplay))))))
