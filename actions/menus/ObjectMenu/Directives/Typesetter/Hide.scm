385c4e9cea82c7ef3ad128ff6aaf3de485524c50
Richard Shann <richard@rshann.plus.com>
2023-09-05 18:22:59 +0100

    Replace corrupted script

--- actions/menus/ObjectMenu/Directives/Typesetter/Hide.scm
+++ actions/menus/ObjectMenu/Directives/Typesetter/Hide.scm
@@ -1,8 +1,3 @@
-(let ()
-(define lilytype (GetTypeAsLilypond))  
-(define lilycontext  (GetContextAsLilypond))
-(if lilytype
-  (StandAloneDirectiveProto (cons "HideNext" (string-append  "\\once \\override " lilycontext "." lilytype " #'stencil = ##f"  )))
-  #f
-)
-)
+(let ((lilytype (GetTypeAsLilypond))  (lilycontext  (GetContextAsLilypond)))
+	(if lilytype
+	  (StandAloneDirectiveProto (cons "HideNext" (string-append  "\\once \\override " lilycontext "." lilytype " #'stencil = ##f"  ))) #f))
