9d367ce953e4e71a7eb7731d24d18e8902ace679
Richard Shann <richard@rshann.plus.com>
2023-09-08 11:41:52 +0100

    Fix ug #64631 numeric sigs command

--- actions/menus/ObjectMenu/TimeSig/NumericTimeSig.scm
+++ actions/menus/ObjectMenu/TimeSig/NumericTimeSig.scm
@@ -1,22 +1,13 @@
 ;;;NumericTimeSig
-(let ((tag "NumericTimeSig")(params NumericTimeSig::params))
-	(if (not params)
+(let ((tag "NumericTimeSig"))
+	(if (d-Directive-timesig? tag)
+		(d-DirectiveDelete-timesig tag)
 		(begin
-			(d-PushPosition)
-			(d-GoToPosition #f 1 #f 1)))
-  (if (d-Directive-timesig? tag)
-    (d-DirectiveDelete-timesig tag)
-    (begin
-      (d-DirectivePut-timesig-prefix tag "\\numericTimeSignature\n")
-      (d-DirectivePut-timesig-graphic tag "\nN\nDenemo\n12")
-      (d-DirectivePut-timesig-gy tag 0)
-      (d-DirectivePut-timesig-minpixels tag 30)))
-
-  
-  (while (MoveDownStaffOrVoice)
-  	(d-NumericTimeSig 'recursive))
- (if (not params)
-	(begin
-		(d-PopPosition)
-		(d-SetSaved #f)
-		(d-RefreshDisplay))))
+		  (d-DirectivePut-timesig-prefix tag "\\numericTimeSignature\n")
+		  (d-DirectivePut-timesig-graphic tag "\nN\nDenemo\n12")
+		  (d-DirectivePut-timesig-gy tag 0)
+		  (d-DirectivePut-timesig-minpixels tag 30)))
+		  
+	(if (d-Directive-timesig? tag)
+		(d-WarningDialog (_ "Time signatures will typeset as numeric from this moment in the score"))
+		(d-WarningDialog (_ "Removed numeric time signatures directive"))))
