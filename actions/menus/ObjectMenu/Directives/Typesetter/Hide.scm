8f8c93cf214c80a11f0fca3d9c36fcc0016286c0
Richard Shann <richard@rshann.plus.com>
2023-09-28 19:30:17 +0100

    Warn if trying to hide standalone

--- actions/menus/ObjectMenu/Directives/Typesetter/Hide.scm
+++ actions/menus/ObjectMenu/Directives/Typesetter/Hide.scm
@@ -1,3 +1,5 @@
 (let ((lilytype (GetTypeAsLilypond))  (lilycontext  (GetContextAsLilypond)))
-	(if lilytype
-	  (StandAloneDirectiveProto (cons "HideNext" (string-append  "\\once \\override " lilycontext "." lilytype " #'stencil = ##f"  ))) #f))
+	(if (d-Directive-standalone?)
+		(d-WarningDialog (_ "Use Conditional Directives menu to hide this directive"))
+		(if lilytype
+			(StandAloneDirectiveProto (cons "HideNext" (string-append  "\\once \\override " lilycontext "." lilytype ".stencil = ##f"  ))) #f)))
