b1cf73c546e2a5a00fe930ac95bf8c72fe92f303
Richard Shann <richard@rshann.plus.com>
2024-03-11 15:08:12 +0000

    Make Piano staff name a separate directive on voice

--- actions/menus/ObjectMenu/StaffMenu/StaffPropertiesMenu/PianoStaffName.scm
+++ actions/menus/ObjectMenu/StaffMenu/StaffPropertiesMenu/PianoStaffName.scm
@@ -1,14 +1,13 @@
 ;;;PianoStaffName
-(let ((tag "PianoStaffStart") (name "Piano")(shortName "Pno"))
-	(if (d-DirectiveGet-staff-prefix tag)
+(let ((stag "PianoStaffStart") (tag "PianoStaffName")(name "Piano")(shortName "Pno"))
+	(if (d-DirectiveGet-staff-prefix stag)
 		(begin
 		    (set! name (d-GetUserInput (_ "Instrument Name") (_ "Give name of instrument for staff group starting here:") name))
 		    (set! shortName (d-GetUserInput (_ "Short Instrument Name") (_ "Give short name of instrument for staff group starting here:") shortName))
 		    (if (not shortName)
 		    			(set! shortName ""))
 		    (if name
-		    	(set! name (string-append "\\set PianoStaff.instrumentName = #\"" name "\" " "\\set PianoStaff.shortInstrumentName = #\"" shortName "\" "))
-			(set! name ""))
-		    (AttachDirective "staff"  "prefix"  "PianoStaffStart" (string-append " \\new PianoStaff << " name " \n") DENEMO_OVERRIDE_GRAPHIC DENEMO_OVERRIDE_AFFIX))
-		  (begin
-		  	(d-WarningDialog "This command must be issued on the first staff of a piano staff group"))))
+					(d-DirectivePut-voice-postfix tag (string-append "\\set PianoStaff.instrumentName = #\"" name 
+							"\" " "\\set PianoStaff.shortInstrumentName = #\"" shortName "\" "))
+					(d-WarningDialog (_ "Cancelled"))))
+		(d-WarningDialog "This command must be issued on the first staff of a piano staff group")))
