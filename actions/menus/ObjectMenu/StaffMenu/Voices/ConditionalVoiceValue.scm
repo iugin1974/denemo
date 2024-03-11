1238875447295ff722d1d33b298723e50954faf4
Richard Shann <richard@rshann.plus.com>
2024-03-11 15:26:40 +0000

    Create command for conditional voice properties and bump version for it

---
+++ actions/menus/ObjectMenu/StaffMenu/Voices/ConditionalVoiceValue.scm
@@ -0,0 +1,4 @@
+;;ConditionalVoiceValue
+;label: Conditional Values/Properties
+;tooltip: Allows you to create conditional values for properties ("directives") you have set on the current voice Use e.g. use to set a different Instrument Name when an Inclusion Criterion is set in the Print View.
+(ConditionalValue "voice")
