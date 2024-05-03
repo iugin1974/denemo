2ac9a3a492272d30aed84f635f1dd0b57e28bab4
Richard Shann <richard@rshann.plus.com>
2024-05-03 13:59:06 +0100

    Run non-printing voice when non-printing staff requested on a voice

--- actions/menus/ObjectMenu/StaffMenu/StaffPropertiesMenu/NonPrintingStaff.scm
+++ actions/menus/ObjectMenu/StaffMenu/StaffPropertiesMenu/NonPrintingStaff.scm
@@ -1,21 +1,23 @@
 ;;;NonPrintingStaff
-(let ((tag "NonPrintingStaff")(params NonPrintingStaff::params));(disp "params " params " so eq set? " (eq? params 'set) (eq? params 'unset) "\n")
-     (if (eq? params 'unset)
-     	(begin ;(disp "deleting for " params "\n") 
-     		(d-DirectiveDelete-staff tag))
-     	(begin
-     		(if (eq? params 'set)
-     			(d-DirectiveDelete-staff tag))
-		(if (d-DirectiveGetForTag-staff tag)
-			(begin
-				(if (not params)
-					(d-InfoDialog (_ "Staff will be printed")))
-				(d-DirectiveDelete-staff tag))
-			(begin 
-				(d-DirectivePut-staff-prefix tag " \\void { ")
-				(d-DirectivePut-staff-postfix tag " }\n ")
-				(d-DirectivePut-staff-override tag  (logior DENEMO_ALT_OVERRIDE DENEMO_OVERRIDE_GRAPHIC))
-				(d-DirectivePut-staff-display tag "Hidden Staff")
-				(if (not params)
-					(SetDirectiveConditional "staff"  tag))))
-				(d-SetSaved #f))))
+(if (d-IsVoice)
+	(d-NonPrintingVoice)
+	(let ((tag "NonPrintingStaff")(params NonPrintingStaff::params));(disp "params " params " so eq set? " (eq? params 'set) (eq? params 'unset) "\n")
+	     (if (eq? params 'unset)
+	     	(begin ;(disp "deleting for " params "\n") 
+	     		(d-DirectiveDelete-staff tag))
+	     	(begin
+	     		(if (eq? params 'set)
+	     			(d-DirectiveDelete-staff tag))
+			(if (d-DirectiveGetForTag-staff tag)
+				(begin
+					(if (not params)
+						(d-InfoDialog (_ "Staff will be printed")))
+					(d-DirectiveDelete-staff tag))
+				(begin 
+					(d-DirectivePut-staff-prefix tag " \\void { ")
+					(d-DirectivePut-staff-postfix tag " }\n ")
+					(d-DirectivePut-staff-override tag  (logior DENEMO_ALT_OVERRIDE DENEMO_OVERRIDE_GRAPHIC))
+					(d-DirectivePut-staff-display tag "Hidden Staff")
+					(if (not params)
+						(SetDirectiveConditional "staff"  tag))))
+					(d-SetSaved #f)))))
