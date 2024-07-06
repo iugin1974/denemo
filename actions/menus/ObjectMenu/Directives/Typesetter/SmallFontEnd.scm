62f226651b4014394902e61810e33556204a7930
Richard Shann <richard@rshann.plus.com>
2024-07-06 09:25:20 +0100

    Small music should include rests and dynamics

--- actions/menus/ObjectMenu/Directives/Typesetter/SmallFontEnd.scm
+++ actions/menus/ObjectMenu/Directives/Typesetter/SmallFontEnd.scm
@@ -1,5 +1,7 @@
 ;;;SmallFontEnd
-     (StandAloneDirectiveProto (cons "SmallFontEnd" "%Restore normal note size
+     (StandAloneDirectiveProto (cons "SmallFontEnd" "%Restore normal music size
+        	\\revert Rest.font-size         	
+        	\\revert DynamicText.font-size 
         	\\revert NoteHead.font-size 
         	\\revert Accidental.font-size
 ") #t "\ns")
--- actions/menus/ObjectMenu/Directives/Typesetter/SmallFontStart.scm
+++ actions/menus/ObjectMenu/Directives/Typesetter/SmallFontStart.scm
@@ -1,5 +1,7 @@
 ;;;SmallFontStart
-(StandAloneDirectiveProto (cons "SmallFontStart" "% Start small notes		
+(StandAloneDirectiveProto (cons "SmallFontStart" "% Start small music		
+		\\override Rest.font-size = #-4	
+		\\override DynamicText.font-size = #-4
 		\\override NoteHead.font-size = #-4
 		\\override Accidental.font-size = #-4
 ") #t "\nt")
