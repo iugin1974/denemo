034dc1b8442663932a96a96c265ed973ce1f29f0
Richard Shann <richard@rshann.plus.com>
2024-10-21 17:22:04 +0100

    Improve help for Transpose on Playback

--- actions/menus/ObjectMenu/MovementMenu/Playback/TransposeOnPlayback.scm
+++ actions/menus/ObjectMenu/MovementMenu/Playback/TransposeOnPlayback.scm
@@ -9,7 +9,7 @@
 (if (not params)
 	(let ((current  (string->number  (d-StaffProperties "query=transposition"))))
 		(set! current (number->string (- current)))
-		(set! interval (d-GetUserInput (_ "Playback Transposition") (_ "Give semitones (+/-)") 
+		(set! interval (d-GetUserInput (_ "Relative Playback Transposition (relative to current value)") (_ "Give semitones (+/-)") 
 			 current ))))
 		
 (if (number? interval)
