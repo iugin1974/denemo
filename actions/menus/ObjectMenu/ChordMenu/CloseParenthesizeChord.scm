ec3eb839d1de5aaae3f7a9901725e621954a583d
Richard Shann <richard@rshann.plus.com>
2024-11-04 12:01:08 +0000

    Update chord paren for LilyPond 2.24

--- actions/menus/ObjectMenu/ChordMenu/CloseParenthesizeChord.scm
+++ actions/menus/ObjectMenu/ChordMenu/CloseParenthesizeChord.scm
@@ -4,9 +4,13 @@
  	(d-DirectiveDelete-chord  tag)
  	(begin
 		(d-LilyPondDefinition (cons "endParenthesize"
+		(if (d-CheckLilyVersion "2.24.0")
+" \\once \\override Parentheses.stencils = #(lambda (grob)
+        (let ((par-list (parentheses-interface::calc-parenthesis-stencils grob)))
+          (list point-stencil (cadr par-list))))"			
 " \\once \\override ParenthesesItem.stencils = #(lambda (grob)
         (let ((par-list (parentheses-item::calc-parenthesis-stencils grob)))
-          (list point-stencil (cadr par-list))))"))
+          (list point-stencil (cadr par-list))))")))
 		(d-DirectivePut-chord-prefix tag "\\endParenthesize\\parenthesize ")
 		(d-DirectivePut-chord-override tag DENEMO_OVERRIDE_AFFIX)
 		(d-DirectivePut-chord-display tag ")")))
--- actions/menus/ObjectMenu/ChordMenu/OpenParenthesizeChord.scm
+++ actions/menus/ObjectMenu/ChordMenu/OpenParenthesizeChord.scm
@@ -4,9 +4,13 @@
     (d-DirectiveDelete-chord  tag)
     (begin
         (d-LilyPondDefinition (cons "startParenthesize" 
- "\\once \\override ParenthesesItem.stencils = #(lambda (grob)
+		(if (d-CheckLilyVersion "2.24.0")
+" \\once \\override Parentheses.stencils = #(lambda (grob)
+        (let ((par-list (parentheses-interface::calc-parenthesis-stencils grob)))
+          (list (car par-list) point-stencil)))"        
+" \\once \\override ParenthesesItem.stencils = #(lambda (grob)
         (let ((par-list (parentheses-item::calc-parenthesis-stencils grob)))
-          (list (car par-list) point-stencil )))"))
+          (list (car par-list) point-stencil )))")))
         (d-DirectivePut-chord-prefix tag "\\startParenthesize\\parenthesize ")
         (d-DirectivePut-chord-override tag DENEMO_OVERRIDE_AFFIX)
         (d-DirectivePut-chord-display tag "(")))
