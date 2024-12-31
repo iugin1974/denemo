d31966d9da7f4c26f04a48b369b4d09012795f24
Richard Shann <richard@rshann.plus.com>
2024-12-31 16:59:47 +0000

    Use uniform syntax for Piano name as Instrument name

--- actions/menus/ObjectMenu/StaffMenu/StaffPropertiesMenu/PianoStaffName.scm
+++ actions/menus/ObjectMenu/StaffMenu/StaffPropertiesMenu/PianoStaffName.scm
@@ -1,5 +1,5 @@
 ;;;PianoStaffName
-(let ((stag "PianoStaffStart") (tag "PianoStaffName")(name "Piano")(shortName "Pno"))
+(let ((tag "PianoStaffName")   (tag2 "PianoStaffShortName") (stag "PianoStaffStart")  (name "Piano")(shortName "Pno"))
 	(if (d-DirectiveGet-staff-prefix stag)
 		(begin
 		    (set! name (d-GetUserInput (_ "Instrument Name") (_ "Give name of instrument for staff group starting here:") name))
@@ -7,7 +7,9 @@
 		    (if (not shortName)
 		    			(set! shortName ""))
 		    (if name
-					(d-DirectivePut-voice-postfix tag (string-append "\\set PianoStaff.instrumentName = #\"" name 
-							"\" " "\\set PianoStaff.shortInstrumentName = #\"" shortName "\" "))
+					(begin
+					 (d-DirectivePut-voice-postfix tag (string-append "\\set PianoStaff.instrumentName = \\markup \"" name 
+							"\" " ))
+					(d-DirectivePut-voice-postfix tag2 (string-append "\\set PianoStaff.shortInstrumentName = \\markup \"" shortName "\" ")))
 					(d-WarningDialog (_ "Cancelled"))))
 		(d-WarningDialog "This command must be issued on the first staff of a piano staff group")))
